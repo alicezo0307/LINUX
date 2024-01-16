30fc9a5cab5d02ee3d32ef3796fdcac8a0fa5adb
alicezo0307 <151118219+alicezo0307@users.noreply.github.com>
2024-01-15 20:32:25 -0800

    Update 6_3.py

--- lab6/6_3.py
+++ lab6/6_3.py
@@ -1,1 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+SW = 22
+LED = 18
+count = 0
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(SW, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+try:
+    while True :
+        if GPIO.wait_for_edge(SW, GPIO.FALLING):
+            count = count+1
+            GPIO.output(LED, True)
+            print("on")
+            while GPIO.wait_for_edge(SW, GPIO.FALLING):
+                GPIO.output(LED, False)
+                print("off")
+                break
+            
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+print("\nBye..")
